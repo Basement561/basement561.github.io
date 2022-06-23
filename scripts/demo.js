192edeeaa955572a8356d17a2912715b30811b99
Basement561 <personnamehere29@gmail.com>
2022-06-23 12:45:31 +1000

    fixed text and removed .css file

--- scripts/demo.js
+++ scripts/demo.js
@@ -3,14 +3,3 @@ const testHeading = document.getElementById('p1');
 
 const button = document.getElementById('pb1');
 button.onclick = function(){alert('JS is working.')};
-
-function delay(milliseconds){
-    return new Promise(resolve => {
-        setTimeout(resolve, milliseconds);
-    });
-}
-
-while (1==1) {
-    alert('JS is working');
-    await delay(7000);
-}
